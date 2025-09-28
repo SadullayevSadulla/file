1e3308c82279024e15b00b0a0d36a6be6a5ace10
SadullayevSadulla <sadullayev813@gmail.com>
2025-09-28 20:06:24 +0500

    asdfghjk

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -8,12 +8,14 @@ import Aloqa from "../components/Aloqa/Aloqa.jsx";
 import Header from "../all/header/header.jsx";
 import Footer from "../components/footer/footer.jsx";
 import Fikir from "../components/Fikirlar/Fikir.jsx";
+import Kimbu from "../components/kimbu/Kimbu.jsx";
 const App = () => {
   return (
     <>
       <Header />
       <BoshSahifa />
       <BizHaqimizda />
+      <Kimbu/>
       <Jamoa />
       <Afzalliklar />
       <Fikir />
