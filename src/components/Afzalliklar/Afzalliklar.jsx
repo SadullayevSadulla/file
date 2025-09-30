533997b9b2d7051bcbc61312acd179985fb4ee79
SadullayevSadulla <sadullayev813@gmail.com>
2025-09-30 16:24:45 +0500

    salom dunyo

--- src/components/Afzalliklar/Afzalliklar.jsx
+++ src/components/Afzalliklar/Afzalliklar.jsx
@@ -1,92 +1,201 @@
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
+// import React from 'react'
+// import { Swiper, SwiperSlide } from 'swiper/react'
+// import 'swiper/css'
+
+// const Afzalliklar = () => {
+//   return (
+//     <>
+//       <div className="container">
+//         <div className="section_card" id="afzalliklar">
+//           <div className="card_h1">
+//             <div className="main_main_btn1">
+//               <button>Nega bizni tanladingiz</button>
+//             </div>
+//             <h1 className="h11">
+//               Bizning markazimiz innovatsion va interaktiv ta'lim metodikalariga ega
+//             </h1>
+//             <img className="img7" src="/Vector 25 (1).svg" alt="img" />
+//           </div>
+//           <Swiper className="mySwiper mainSwiper">
+//             <SwiperSlide>
+//               <div className="card">
+//                 <div className="icon">
+//                   <img src="/technology_svgrepo.com.svg" alt="img" />
+//                 </div>
+//                 <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+//                 <p className="text">
+//                   Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+//                 </p>
+//               </div>
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <div className="card">
+//                 <div className="icon">
+//                   <img src="/Vector (8).svg" alt="img" />
+//                 </div>
+//                 <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+//                 <p className="text">
+//                   Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+//                 </p>
+//               </div>
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <div className="card">
+//                 <div className="icon">
+//                   <img src="/control-multimedia-player_svgrepo.com.svg" alt="img" />
+//                 </div>
+//                 <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+//                 <p className="text">
+//                   Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+//                 </p>
+//               </div>
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <div className="card">
+//                 <div className="icon">
+//                   <img src="/control-discipline-moderation_svgrepo.com.svg" alt="img" />
+//                 </div>
+//                 <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+//                 <p className="text">
+//                   Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+//                 </p>
+//               </div>
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <div className="card">
+//                 <div className="icon">
+//                   <img src="/XMLID_129_.svg" alt="img" />
+//                 </div>
+//                 <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+//                 <p className="text">
+//                   Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+//                 </p>
+//               </div>
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <div className="card">
+//                 <div className="icon">
+//                   <img src="/library-book_svgrepo.com.svg" alt="img" />
+//                 </div>
+//                 <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+//                 <p className="text">
+//                   Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+//                 </p>
+//               </div>
+//             </SwiperSlide>
+//           </Swiper>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Afzalliklar
+
+import React from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
 
 const Afzalliklar = () => {
   return (
-    <>
-      <div className="container">
-        <div className="section_card" id="afzalliklar">
-          <div className="card_h1">
-            <div className="main_main_btn1">
-              <button>Nega bizni tanladingiz</button>
-            </div>
-            <h1 className="h11">
-              Bizning markazimiz innovatsion va interaktiv ta'lim metodikalariga ega
-            </h1>
-            <img className="img7" src="/Vector 25 (1).svg" alt="img" />
+    <div className="container">
+      <div className="section_card" id="afzalliklar">
+        <div className="card_h1">
+          <div className="main_main_btn1">
+            <button>Nega bizni tanladingiz</button>
           </div>
-          <Swiper className="mySwiper mainSwiper">
-            <SwiperSlide>
-              <div className="card">
-                <div className="icon">
-                  <img src="/technology_svgrepo.com.svg" alt="img" />
-                </div>
-                <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
-                <p className="text">
-                  Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
-                </p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="card">
-                <div className="icon">
-                  <img src="/Vector (8).svg" alt="img" />
-                </div>
-                <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
-                <p className="text">
-                  Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
-                </p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="card">
-                <div className="icon">
-                  <img src="/control-multimedia-player_svgrepo.com.svg" alt="img" />
-                </div>
-                <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
-                <p className="text">
-                  Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
-                </p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="card">
-                <div className="icon">
-                  <img src="/control-discipline-moderation_svgrepo.com.svg" alt="img" />
-                </div>
-                <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
-                <p className="text">
-                  Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
-                </p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="card">
-                <div className="icon">
-                  <img src="/XMLID_129_.svg" alt="img" />
-                </div>
-                <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
-                <p className="text">
-                  Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
-                </p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="card">
-                <div className="icon">
-                  <img src="/library-book_svgrepo.com.svg" alt="img" />
-                </div>
-                <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
-                <p className="text">
-                  Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
-                </p>
-              </div>
-            </SwiperSlide>
-          </Swiper>
+          <h1 className="h11">
+            Bizning markazimiz innovatsion va interaktiv ta'lim metodikalariga ega
+          </h1>
+          <img className="img7" src="/Vector 25 (1).svg" alt="img" />
         </div>
+
+        <Swiper
+          className="mySwiper mainSwiper"
+          slidesPerView={"auto"}
+          spaceBetween={20}
+          centeredSlides={true}
+          grabCursor={true}
+          breakpoints={{
+            320: { slidesPerView: 1, centeredSlides: true },
+            768: { slidesPerView: 2, centeredSlides: false },
+            1024: { slidesPerView: 3, centeredSlides: false },
+          }}
+        >
+          <SwiperSlide>
+            <div className="card">
+              <div className="icon">
+                <img src="/technology_svgrepo.com.svg" alt="img" />
+              </div>
+              <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+              <p className="text">
+                Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+              </p>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="card">
+              <div className="icon">
+                <img src="/Vector (8).svg" alt="img" />
+              </div>
+              <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+              <p className="text">
+                Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+              </p>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="card">
+              <div className="icon">
+                <img src="/control-multimedia-player_svgrepo.com.svg" alt="img" />
+              </div>
+              <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+              <p className="text">
+                Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+              </p>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="card">
+              <div className="icon">
+                <img src="/control-discipline-moderation_svgrepo.com.svg" alt="img" />
+              </div>
+              <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+              <p className="text">
+                Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+              </p>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="card">
+              <div className="icon">
+                <img src="/XMLID_129_.svg" alt="img" />
+              </div>
+              <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+              <p className="text">
+                Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+              </p>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="card">
+              <div className="icon">
+                <img src="/library-book_svgrepo.com.svg" alt="img" />
+              </div>
+              <h2 className="title">Innovatsion va sifatli o'qitish uslublari</h2>
+              <p className="text">
+                Markazimizda innovatsion metodikalarga asoslangan sifatli ta'lim beriladi.
+              </p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
-    </>
+    </div>
   )
 }
 
